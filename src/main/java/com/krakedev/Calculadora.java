f97ca11e254f0fe74dade9ce1a84ae0d189fba36
OscarG-08 <oscarg-ricardos@hotmail.com>
2023-12-07 18:50:45 -0500

    Se agrego las clases Calculadora y TestCalculadora, ademas se agrego un metodo multiplicar en la clase Calculadora

---
+++ src/main/java/com/krakedev/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.krakedev;
+
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Calculadora {
+    public double multiplicar(double valor1, double valor2){
+        return valor1*valor2;
+    }
+    
+}
