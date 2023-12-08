e9e5c8597ae6d6888a57cd8f9304832b904559e6
OscarG-08 <oscarg-ricardos@hotmail.com>
2023-12-07 19:42:39 -0500

    Se agrego la clase Rectangulo con el metodo del perimetro y se agrego la clase TestRectangulo para probar el mismo

---
+++ src/main/java/com/krakedev/Rectangulo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.krakedev;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Rectangulo {
+    private double base;
+    private double altura;
+
+    public Rectangulo() {
+    }
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    public double calcularPerimetro(){
+        double perimetro = (this.base + this.altura) * 2;
+        return perimetro;
+    } 
+}
